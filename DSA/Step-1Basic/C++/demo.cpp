b90b4ebad549381329e9700bc075732b5ca511a0
Aman Adhikari <noattacklost@gmail.com>
2025-06-06 21:22:34 +0530

    "done"

--- DSA/Step-1Basic/C++/demo.cpp
+++ DSA/Step-1Basic/C++/demo.cpp
@@ -123,25 +123,43 @@ using namespace std;
     //     }
     // }
 
-    void print3(int n){
-        
-        for(int i=n-1; i >= 0; i--){
-            //space
-            for(int j = 0; j < n-i; j++){
-                cout << " ";
-            }
-            for(int j = 0; j < 2*i+1; j++){
-                cout << "*";
-            }
-            // star
-            // space
-            for(int j = 0; j < n-i; j++){
-                cout << " ";
-            }
+    // void print3(int n){
+    //          int half=(n+1)/2;
+    //          for(int i=0; i < half; i++){
+    //         //space
+    //         for(int j = 0; j < half-i+1; j++){
+    //             cout << " ";
+    //         }
+    //         for(int j = 0; j < 2*i+1; j++){
+    //             cout << "*";
+    //         }
+    //         // star
+    //         // space
+    //         for(int j = 0; j < half-i+1; j++){
+    //             cout << " ";
+    //         }
            
-            cout << endl;
-        }
-    }
+    //         cout << endl;
+    //     }
+
+        
+    //     for(int i=n/2-1; i >= 0; i--){
+    //         //space
+    //         for(int j = 0; j < half-i+1; j++){
+    //             cout << " ";
+    //         }
+    //         for(int j = 0; j < 2*i+1; j++){
+    //             cout << "*";
+    //         }
+    //         // star
+    //         // space
+    //         for(int j = 0; j < half-i; j++){
+    //             cout << " ";
+    //         }
+           
+    //         cout << endl;
+    //     }
+    // }
 
     // void print4(int n){
             
@@ -156,11 +174,307 @@ using namespace std;
     //     }
     // }
 
+    // void Diamondhalf(int n){
+    //     int half=(n+1)/2;
+    //     for(int i=0;i < half;i++){
+    //         for(int j=0; j<=i;j++){
+    //               cout << "*";
+    //         }
+    //         cout << endl;
+    //     }
+    //     for(int i=half-1;i>0;i--){
+    //         for(int j=0;j < i;j++){
+    //             cout << "*";
+    //         }
+    //         cout << endl;
+    //     }
+
+    // }
+
+    // void print5(int n){
+    //     int start =1;
+    //      for(int i=0;i< n;i++){
+
+    //         if(i%2 ==0) start =1;
+    //         else start =0;
+
+    //         for(int j=0;j <i+1;j++){
+    //             cout << start;
+    //             start =1 - start;
+    //         }
+    //         cout << endl;
+    //      }
+    // }
+
+    // void print6(int n){
+        
+    //      for(int i=1;i<=n;i++){
+
+    //         for(int j=1;j<=i;j++){
+    //             cout << j;
+
+    //         }
+    //         for(int j=0;j<2*(n-i);j++){
+    //                cout << " ";
+    //         }
+
+    //         for(int j=i;j>=1;j--){
+    //             cout <<j;
+    //         }
+    //         cout << endl;
+    //      }
+    // }
+    // void print7(int n){
+    //     int start=0;
+    //      for(int i=1;i<=n;i++){
+            
+    //         for(int j=1;j<=i;j++){
+    //             cout << start+1;
+    //             start++;
+    //             cout << " ";
+
+    //         }
+               
+    //         cout << endl;
+    //      }
+    // }
+    // void print8(int n){
+        
+    //      for(int i=0;i<n;i++){
+            
+    //      for(char ch= 'A'; ch <='A'+i; ch++){
+    //              cout << ch;
+    //      }
+               
+    //         cout << endl;
+    //      }
+    // }
+    // void print9(int n){
+        
+    //      for(int i=n;i>=0;i--){
+            
+    //      for(char ch= 'A'; ch <'A'+i; ch++){
+    //              cout << ch;
+    //      }
+               
+    //         cout << endl;
+    //      }
+    // }
+    // void print10(int n){
+    //      char letter='A';
+    //      for(int i=1;i<=n;i++){
+            
+    //      for(char ch= 'A'; ch <'A'+i; ch++){
+    //              cout << letter;
+    //      }
+    //          letter='A'+i;
+               
+    //         cout << endl;
+    //      }
+    // }
+
+    // void print11(int n){
+    //     for(int i=0;i < n;i++){
+
+    //      for(int j=0;j<n-i-1;j++){
+    //         cout << " ";
+    //      }
+    //      //character
+    //      char ch= 'A';
+    //      int breakpoint =(2*i+1)/2;
+    //      for(int j=0;j<2*i+1;j++){
+    //           cout << ch;
+    //          if( j >= breakpoint){
+    //             ch--;
+    //          }else{
+    //             ch++;
+    //          }
+    //      }
+
+    //      for(int j=0;j<n-i-1;j++){
+    //         cout << " ";
+    //      }
+               
+    //         cout << endl;
+    //      }
+    // }
+
+    // void print12(int n){
+        
+    //     for(int i=0;i< n;i++){
+
+    //         for(char ch='E'-i; ch <= 'E';ch++){
+    //               cout << ch << " ";
+    //         }
+    //         cout << endl;
+    //     }
+    // }
+
+    // void print13(int n){
+    //          int half=(n+1)/2;
+    //          for(int i=0; i < half; i++){
+    //         //space
+    //         for(int j = 0; j < half-i; j++){
+    //             cout << "*";
+    //         }
+    //         for(int j = 0; j < 2*i; j++){
+    //             cout << " ";
+    //         }
+    //         // star
+    //         // space
+    //         for(int j = 0; j < half-i; j++){
+    //             cout << "*";
+    //         }
+           
+    //         cout << endl;
+    //     }
+
+        
+    //     for(int i=n/2-1; i >= 0; i--){
+    //         //space
+    //         for(int j = 0; j < half-i; j++){
+    //             cout << "*";
+    //         }
+    //         for(int j = 0; j < 2*i; j++){
+    //             cout << " ";
+    //         }
+    //         // star
+    //         // space
+    //         for(int j = 0; j < half-i; j++){
+    //             cout << "*";
+    //         }
+           
+    //         cout << endl;
+    //     }
+    // }
+
+    // void print13(int n){
+
+    //     int spc=0;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=1;j<=n-i;j++){
+    //             cout << "*";
+    //         }
+    //         for( int j=0; j<spc;j++){
+    //             cout << " ";
+    //         }
+
+    //         for(int j=1;j<=n-i;j++){
+    //             cout << "*";
+    //         }
+
+    //         spc+=2;
+    //         cout << endl;
+    //     }
+    //      int space2=8;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<=i;j++){
+    //             cout << "*";
+    //         }
+    //          for( int j=0; j<space2;j++){
+    //             cout << " ";
+    //         }
+    //         for(int j=0;j<=i;j++){
+    //             cout << "*";
+    //         }
+            
+    //         space2-=2;
+    //         cout << endl;
+    //     }
+    // }
+
+    // void print14(int n){
+    //     int spaces=2 * (n - 1);
+       
+    //     for(int i=0;i < n;i++){
+
+    //         for(int j=0; j<=i;j++){
+    //             cout << "*";
+    //         }
+    //         for(int j=0; j<spaces;j++){
+    //             cout << " ";
+    //         }
+    //          for(int j=0; j<=i;j++){
+    //             cout << "*";
+    //         }
+    //         spaces-=2;
+    //         cout << endl;
+    // }
+            
+    //         int spaces2=2;
+           
+    //         for(int i=1;i<n;i++){
+                
+    //             for(int j=0;j<n-i;j++){
+    //                cout << "*";
+    //             }
+    //             for(int j=0;j<spaces2;j++){
+    //                 cout << " ";
+    //             }
+    //             for(int j=0;j<n-i;j++){
+    //                cout << "*";
+    //             }
+    //             spaces2 +=2;
+    //             cout << endl;
+                
+    //         }
+        
+    // }
+
+    // void print15(int n){
+
+    //     //upper half
+    //     int spaces2=0;
+    //     for(int i=0;i<n;i++){
+
+    //         for(int j=n-i;j>0;j--){
+    //             cout << "*";
+    //         }
+    //         for(int j=0;j< spaces2;j++){
+    //             cout << " ";
+    //         }
+    //         for(int j=n-i;j>0;j--){
+    //             cout << "*";
+    //         }
+    //         spaces2+=2;
+    //         cout << endl;
+    //     }
+
+
+
+    //     // Lower half
+    //     int spaces=n*2-2;
+    //     for(int i=0;i<n;i++){
+
+    //         for(int j=0;j<=i;j++){
+    //             cout << "*";
+    //         }
+    //         for(int j=0;j<spaces;j++){
+    //             cout << " ";
+    //         }
+    //         for(int j=0;j<=i;j++){
+    //             cout << "*";
+    //         }
+    //         spaces-=2;
+    //         cout << endl;
+    //     }
+    // }
+
+    void print16( int n){
+        for(int i=0;i<n;i++){
+            for(int j =0;j<n;j++){
+              if(i==0 || j==0 || i == n-1 || j== n-1){
+                cout << "*";
+              }else cout << " ";
+            }
+            cout << endl;
+        }
+    }
 
 int main(){
     int x;
     cin >> x;
-    print3(x);
+    print16(x);
 
 }
 
